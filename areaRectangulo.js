e6853c9569da6257415eecbe34d036973bd0239a
Lucia Moyano Tello <luciaamancay@gmail.com>
2023-05-27 14:13:09 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,10 @@
+let base;
+let altura;
+base = 5;
+altura = 3;
+let area = base * altura
+console.log(area); // 15
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log(area); //66.25
---
+++ promedio3.js
@@ -0,0 +1,18 @@
+let x;
+let y;
+let z;
+x = 5;
+y = 7;
+z = 9;
+let promedio = (x + y + z) / 3;
+console.log(promedio); //7
+x = -2;
+y = -3;
+z = -8;
+promedio = (x + y + z) / 3;
+console.log(promedio); //-4,33...
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x + y + z) / 3;
+console.log(promedio); //0.266...
