147f4328cd9a4dfcf7ecff5171318028710c792e
Lucia Moyano Tello <luciaamancay@gmail.com>
2023-05-27 13:16:23 -0300

    Agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,7 @@
+let fahrenheit;
+fahrenheit = 50;
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);//10
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);//23,8888888889
